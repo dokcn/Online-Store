aa37b21e63f9ca1e434d1b689e0e9897695bb61a
dokcn <peser@live.com>
2023-11-08 11:18:08 +0800

    配置uni-ui

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-//
+function change() {}
 </script>
 
 <template>
-  <view class="index">index</view>
+  <view class="index">
+    <uni-calendar @change="change" />
+  </view>
 </template>
 
 <style lang="scss">
